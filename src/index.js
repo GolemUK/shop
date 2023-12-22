d5d4f6fc5d6305191d19e077530472d909c9b3c7
Михаил <mixashelby229@gmail.com>
2023-12-23 01:59:24 +0200

    save

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css'; // Якщо у вас є стилі
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
