d8ec98d4b2194f9bd08e28f32ec5418d2c3260a0
Михаил <mixashelby229@gmail.com>
2023-12-27 15:08:35 +0200

    pushds

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './index.css'; // Якщо у вас є стилі
+ // Якщо у вас є стилі
 
 ReactDOM.render(
   <React.StrictMode>
